03bdac15e7e19c80ffac973abb01e1cc6990df45
yukeshwarp <Yukeshwarp@docu3c.com>
2024-10-16 08:08:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import logging
 import io
 
 # Add your logo at the top of the app
-st.logo("logo.png", icon_image=main_body_logo)  # Adjust the width to suit your design
+st.logo("logo.png", icon_image="logo.png")  # Adjust the width to suit your design
 
 # Initialize session state variables
 if 'documents' not in st.session_state:
