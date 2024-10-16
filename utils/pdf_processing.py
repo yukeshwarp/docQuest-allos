a4388872ea0bef5c3af27e022b9e2598d9877e5e
yukeshwarp <Yukeshwarp@docu3c.com>
2024-10-16 11:17:28 +0530

    Update pdf_processing.py

--- utils/pdf_processing.py
+++ utils/pdf_processing.py
@@ -34,40 +34,36 @@ def compress_image(image_data, max_size=(1024, 1024), quality=55):
         logging.error(f"Error compressing image: {e}")
         return image_data  # Return original data if compression fails
 
-def detect_ocr_images_and_vector_graphics_in_pdf(pdf_document, ocr_text_threshold=0.19):
+def detect_ocr_images_and_vector_graphics_in_pdf(page, ocr_text_threshold=0.19):
     """Detect pages with OCR images or vector graphics."""
     detected_pages = []
-
-    for page_number in range(len(pdf_document)):
-        page = pdf_document.load_page(page_number)
-        images = page.get_images(full=True)
-        text = page.get_text("text")
-
-        text_blocks = page.get_text("blocks")
-        vector_graphics_detected = any(page.get_drawings())
-        page_area = page.rect.width * page.rect.height
-        text_area = sum((block[2] - block[0]) * (block[3] - block[1]) for block in text_blocks)
-        text_coverage = text_area / page_area
+    images = page.get_images(full=True)
+    text = page.get_text("text")
+    text_blocks = page.get_text("blocks")
+    vector_graphics_detected = any(page.get_drawings())
+    page_area = page.rect.width * page.rect.height
+    text_area = sum((block[2] - block[0]) * (block[3] - block[1]) for block in text_blocks)
+    text_coverage = text_area / page_area
         
-        st.write(text_area)
-        if text_area==0:
+    
+    if text_area==0:
+        pix = page.get_pixmap() 
+        img_data = pix.tobytes("png")
+        pix = None
+        compressed_img_data = compress_image(img_data)
+        base64_image = base64.b64encode(compressed_img_data).decode("utf-8")
+        detected_pages.append((page_number + 1, base64_image))
+        
+    elif (images or vector_graphics_detected) and text.strip():
+        if text_coverage < ocr_text_threshold:
             pix = page.get_pixmap() 
             img_data = pix.tobytes("png")
             pix = None
             compressed_img_data = compress_image(img_data)
             base64_image = base64.b64encode(compressed_img_data).decode("utf-8")
             detected_pages.append((page_number + 1, base64_image))
-            
-        elif (images or vector_graphics_detected) and text.strip():
-            if text_coverage < ocr_text_threshold:
-                pix = page.get_pixmap() 
-                img_data = pix.tobytes("png")
-                pix = None
-                compressed_img_data = compress_image(img_data)
-                base64_image = base64.b64encode(compressed_img_data).decode("utf-8")
-                detected_pages.append((page_number + 1, base64_image))
                 
-    return detected_pages
+return detected_pages
 
 
 def process_page_batch(pdf_document, batch, system_prompt, ocr_text_threshold=0.4):
